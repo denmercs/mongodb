df479cfaf68b21225f82d66bd417dd2ed5587312
denmercs <denmercs@gmail.com>
2020-09-01 04:55:11 -0500

    added middleware

---
+++ test/middleware_test.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const assert = require("assert");
+const User = require("../src/user");
+const BlogPost = require("../src/blogPost");
+
+describe("Middleware", () => {});
